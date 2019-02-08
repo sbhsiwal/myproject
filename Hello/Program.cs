62ffddfcb962331b68ddcf0d35ca671933f22620
sbhsiwal <sbhsiwal@gmail.com>
2019-02-08 16:04:42 +0530

    Jenkinscheckout

---
+++ Hello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createmessage());
+
+        }
+     public static string createmessage()
+        {
+
+            return "Hello World";
+        }
+    }
+}
